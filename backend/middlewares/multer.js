7034c5d32222bacb566d1295a6e38f163302c023
monika4445 <monika.zaqaryan02@mail.ru>
2023-06-17 14:15:08 +0400

    newch

---
+++ backend/middlewares/multer.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./static/images");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+module.exports = { storage, upload };
--- backend/validations/product-validation.js
+++ backend/validations/product-validation.js
@@ -1,10 +1,19 @@
 const { body } = require('express-validator');
 
 const productValidationRules = [
-    body('name').notEmpty().withMessage('Name is required'),
-    body('price').isNumeric().withMessage('Price must be a number'),
-    body('description').notEmpty().withMessage('Description is required'),
-    body('quantity').notEmpty().withMessage('Quantity is required').isLength({min:0}).withMessage('Quantity must be greater than or equal to 0'),
-]
+  body('name')
+    .notEmpty().withMessage('Product name is required.'),
+  
+  body('price')
+    .notEmpty().withMessage('Price is required.')
+    .isNumeric().withMessage('Price must be a valid number.'),
+  
+  body('description')
+    .notEmpty().withMessage('Product description is required.'),
+  
+  body('quantity')
+    .notEmpty().withMessage('Quantity is required.')
+    .isInt({ min: 0 }).withMessage('Quantity must be a non-negative integer.'),
+];
 
-module.exports = {productValidationRules}
+module.exports = { productValidationRules };
